39759f3f4105da340a999f0c8aae892be7ad3c58
sr7sidharth <45019932+sr7sidharth@users.noreply.github.com>
2020-09-15 20:19:26 -0700

    core functionality done

--- sentence_analysis.py
+++ sentence_analysis.py
@@ -24,24 +24,31 @@ def sample_classify_text(text_content):
     document = {"content": text_content, "type": type_, "language": language}
 
     response = client.classify_text(document)
+    #print(response)
     # Loop through classified categories returned from the API
-    cat = ""
-    conf = ""
+    result = dict()
+    #cat = ""
+    #conf = ""
     for category in response.categories:
         # Get the name of the category representing the document.
         # See the predefined taxonomy of categories:
         # https://cloud.google.com/natural-language/docs/categories
-        cat += (u"Category name: {}".format(category.name))
+        #print(u"Category name: {}".format(category.name))
+        #cat += (u"Category name: {}".format(category.name))
         # Get the confidence. Number representing how certain the classifier
         # is that this category represents the provided text.
-        conf += (u"Confidence: {}".format(category.confidence))
-    return (cat, conf)
+        #print(u"Confidence: {}".format(category.confidence))
+        #conf += (u"Confidence: {}".format(category.confidence))
+        result[category.name] = category.confidence
+    return result
 
+'''
 def message_list_to_text(message_list):
     result = ""
     for i in message_list:
         result += ".\n" + i
     return result
+'''
 
 client = discord.Client()
 
@@ -53,32 +60,55 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:
         return
-        
+
     if message.content.startswith('$themeAnalysis'):
         messages = await message.channel.history(limit=10000).flatten()
         #msg_content = message.content.strip()
         if message.mentions:
             for user in message.mentions:
-                message_list = []
+                result = dict()
                 for x in messages:
                     if x.author.id == user.id:
-                        message_list.append(x.content)
-                result = message_list_to_text(message_list)
-                if result == ("", ""):
-                    await message.channel.send("Not enough data")
-                else:
-                    await message.channel.send(sample_classify_text(result))
+                        if len(x.content.split()) > 20:
+                            temp_dict = sample_classify_text(x.content)
+                            for key,value in temp_dict.items():
+                                if key in result:
+                                    result[key] = (result[key] + value)/2
+                                elif key not in result:
+                                    result[key] = value
+                sorted_result = sorted(result.items(), key=lambda x : x[1], reverse = True)
+                final = ""
+                index = 0
+                for key,value in sorted_result:
+                    if index == 3:
+                        break
+                    else:
+                        index += 1
+                        final += key + " " + str(value) + "\n"
+                await message.channel.send(final)
         else:
             #use message author messages
-            message_list = []
+            result = dict()
             for x in messages:
                 if x.author.id == message.author.id:
-                    message_list.append(x.content)
-            result = message_list_to_text(message_list)
-            if result == ("", ""):
-                await message.channel.send("Not enough data")
-            else:
-                await message.channel.send(sample_classify_text(result))
+                    if len(x.content.split()) > 20:
+                        temp_dict = sample_classify_text(x.content)
+                        for key,value in temp_dict.items():
+                            if key in result:
+                                result[key] = (result[key] + value)/2
+                            elif key not in result:
+                                result[key] = value
+            sorted_result = sorted(result.items(), key=lambda x : x[1], reverse = True)
+            final = ""
+            index = 0
+            for key,value in sorted_result:
+                if index == 3:
+                    break
+                else:
+                    index += 1
+                    final += key + " " + str(value) + "\n"
+            await message.channel.send(final)
+            
 
 
 client.run('NzU1MTY0Mzc5MzU5NjA4OTEz.X1_Tog.fpHPEYxry0QmUsd6re7YS3FYYjw')
